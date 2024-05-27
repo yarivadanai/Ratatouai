e2f0df0306f4a365201e019ec8c137abb339a269
yarivadanai <yariv.adan.ai@gmail.com>
2024-05-27 21:34:30 +0200

    major refactoring

--- Utils/model_utils.py
+++ Utils/model_utils.py
@@ -1,6 +1,8 @@
 # Standard library imports
 import json
-from typing import Generator, List, Callable
+import logging
+from typing import Generator, List, Callable, Tuple
+
 
 # Third-party library imports
 from anthropic import APIStatusError
@@ -23,6 +25,7 @@ from Utils.streamlit_utils import StreamHandler
 # Constants
 LLAMA3_MODEL = "llama3"
 CLAUDE3_OPUS_MODEL = "claude-3-opus-20240229"
+GPT4O_MODEL=  "gpt-4o"
 
 class ImageAnswer(BaseModel):
     """Model for image analysis results."""
@@ -46,146 +49,184 @@ class RecipeReflection(BaseModel):
     missing: str = Field(description="The list of missing ingredients required by the recipe but not found in the provided list of ingredients.")
     suggestions: str = Field(description="Suggestions on how this recipe can be made healthier.")
 
+class Models:
+    """Class for the RatatouAI brain."""
 
-def get_local_model():
-    """Returns an instance of the Ollama model."""
-    return Ollama(model=LLAMA3_MODEL)
+    def __init__(self, vision_model: Tuple[str,  str], reasoning_model: Tuple[str,  str], formatting_model: Tuple[str,  str]):
+        models={
+            GPT4O_MODEL: ChatOpenAI,
+            CLAUDE3_OPUS_MODEL: ChatAnthropic,
+            LLAMA3_MODEL: Ollama
+        }
+        self.vision = models[vision_model[0]](temperature=0, model_name=vision_model[0], api_key=vision_model[1])
+        self.reasoning = models[reasoning_model[0]](temperature=0, model_name=reasoning_model[0], api_key=reasoning_model[1])
+        self.formatting = models[formatting_model[0]](temperature=0, model_name=formatting_model[0], api_key=formatting_model[1])
 
-def get_claude3_opus(anthropic_key):
-    """Returns an instance of the ChatAnthropic model with a specific model name."""
-    return ChatAnthropic(temperature=0, model_name=CLAUDE3_OPUS_MODEL, api_key=anthropic_key)
-
-def get_chat_gpt(openai_api_key, model_name="gpt-4o"):
-    """Returns an instance of the ChatOpenAI model with a specified model name."""
-    return ChatOpenAI(temperature=0, model_name=model_name, api_key=openai_api_key)
-
-def verify_api_keys(openai_api_key: str, anthropic_api_key: str):
-    """Verifies the API keys and returns True if they are valid."""
-    try:
-        chat = get_chat_gpt(openai_api_key)
-        chat = get_claude3_opus(anthropic_api_key)
-        return True
-    except APIStatusError:
-        return False
-    
-@traceable
-def run_full_chain(images: List[str], stream_handler: StreamHandler):
-    """Runs the full chain of image analysis and chat models."""
-    vision = get_chat_gpt(stream_handler.get_openai_key())
-    claude3_chat = get_claude3_opus(stream_handler.get_anthropic_key())
-    food_items = set()
-
-    stream_handler.write("Looking for food ingredients...", True)
-
-    for img in images:
-        answer = ImageAnswer.parse_raw(analyzeImage(img, vision, stream_handler))
-        for item in answer.answer : 
-            food_items.add(item)
-    
-    if len(food_items) == 0:
-        stream_handler.write("Sorry - couldn't find any food ingredients in the images :(", True)
-        return
-
-    stream_handler.write("Found the following food items:", True)
-    ingredients = ", ".join(food_items)
-    format_ingredients(ingredients, claude3_chat, stream_handler)
-
-    stream_handler.write("RatatouAIng 3 recipes with these ingredients...", True) 
-
-    recipes = getRecipes(ingredients, claude3_chat, stream_handler)
-
-    for recipe in recipes:
-        recipe = recipe.json()
-
-        missing_ingredients_prompt = get_prompt_template(prompts.system_ingredients_gaps_prompt_template, 
-                                                        prompts.human_ingredients_gaps_prompt_templatee)
-        reflection_prompt = get_prompt_template(prompts.system_recipe_reflection_prompt_templatee, 
-                                                prompts.human_recipe_reflection_prompt_template)
-        formatted_recipe_prompt = get_prompt_template(prompts.system_content_formatting_prompt_template, 
-                                                      prompts.human_final_formatting_prompt_template)
-
-        missing_ingredients = ({"recipe": RunnablePassthrough(),"ingredients_list": RunnablePassthrough() } | 
-                               missing_ingredients_prompt | claude3_chat | StrOutputParser())
-        reflection = ({"recipe": RunnablePassthrough()} | reflection_prompt | claude3_chat | StrOutputParser())
-        chain = ({"missing_ingredients": missing_ingredients, "reflection" : reflection, "recipe": RunnablePassthrough()} | 
-                 formatted_recipe_prompt | claude3_chat | StrOutputParser())
-
-        stream_handler.stream(stream_chain(chain, {"recipe": recipe, "ingredients_list": ingredients}), True) 
-    stream_handler.close()
-
-
-@traceable
-def analyzeImage(img: str, chat: BaseLLM, stream_handler: StreamHandler) -> ImageAnswer:
-    """Analyzes an image and returns an ImageAnswer object."""
-    system_message = SystemMessagePromptTemplate.from_template(prompts.system_img2ingredients_prompt_template)
-
-    human_message = HumanMessage(
-        content=[
-            {"type": "text", "text": "Here is an image:\n"},
-            {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{img}", },},
-            {"type": "text", "text": "\n" + prompts.human_img2ingredients_prompt_template}, 
-            ])
-
-    chat_prompt = ChatPromptTemplate.from_messages([system_message, human_message])
-    output_parser = JsonOutputParser(pydantic_object=ImageAnswer)
-    
-    chain = chat_prompt | chat | output_parser
-    return handle_json(stream_handler.stream(stream_chain(chain, {})))
-
-@traceable
-def format_ingredients(ingredients: str, chat: BaseLLM, stream_handler: StreamHandler):
-    """Formats the ingredients and returns the result."""
-    result = execute_chain(prompts.system_content_formatting_prompt_template, 
-                    prompts.human_ingredients_formatting_prompt_template, chat, 
-                    StrOutputParser(), {"ingredients": ingredients}, stream_handler, True)
-    return result
-
-@traceable
-def getRecipes(ingredients: str, chat: BaseLLM, stream_handler: StreamHandler) -> RecipesLists:
-    """Gets recipes based on the ingredients and returns a RecipesLists object."""
-    recipes = execute_chain(prompts.system_ingredients2recipe_prompt_template, 
-                    prompts.human_ingredients2recipe_prompt_template, chat, 
-                    JsonOutputParser(pydantic_object=RecipesLists), 
-                    {"ingredients_list": ingredients, "recipe_answer_outputformat" : prompts.recipeslist_answer_outputformat}, 
-                    stream_handler)
-
-    return RecipesLists.parse_raw(json.dumps(recipes[-1])).recipes_lists
-
-@traceable 
-def get_prompt_template(system_template, human_template):
-    """Gets a prompt template based on the system and human templates."""
-    system_message = SystemMessagePromptTemplate.from_template(system_template)
-    human_message = HumanMessagePromptTemplate.from_template(human_template)
-
-    return ChatPromptTemplate.from_messages([system_message, human_message])
-
-@traceable 
-def execute_chain(system_template: str, human_template: str, chat: BaseLLM, output_parser: Callable, 
-                  params_dict: dict, stream_handler: StreamHandler, persist: bool = False) -> str:
-    """Executes a chain of operations and returns the result."""
-    chat_prompt = get_prompt_template(system_template, human_template)
-    chain = chat_prompt | chat | output_parser
-    return stream_handler.stream(stream_chain(chain, params_dict), persist)
-
-def reply(user_prompt: str, stream_handler: StreamHandler) -> str:
-    """Replies to a user prompt and returns the result."""
-    chat = get_claude3_opus(stream_handler.get_anthropic_key())
-    messages = []
-    last_10_messages = list(stream_handler.messages)#[-10:]
+    def verify_api_keys(self) -> bool:
+        """Verifies the API keys and returns True if they are valid."""
+        try:
+            self.vision.invoke("Hello RatatouAI!")
+            self.reasoning.invoke("Hello RatatouAI!")
+            self.formatting.invoke("Hello RatatouAI!")
+            return True
+        except APIStatusError as e:
+            logging.error("An error occurred:", exc_info=True)
+            return False
+            
+    def get_vision_model(self):
+        """Returns the vision model."""
+        return self.vision
         
-    for m in last_10_messages:
-        if m["type"] == "text":
-            messages.append((m["role"], m["content"]))
+    def get_reasoning_model(self):
+        """Returns the reasoning model."""
+        return self.reasoning
+        
+    def get_formatting_model(self):
+        """Returns the formatting model."""
+        return self.formatting
 
-    promptTmplt = ChatPromptTemplate.from_messages(messages)  
-    chain = promptTmplt | chat | StrOutputParser()
-    return stream_handler.stream(stream_chain(chain, {}), True)
+class RatBrain:
+   
+    """Class for the RatatouAI brain."""
+    def __init__(self, stream_handler: StreamHandler, model: Models):
+        self.vision_model = model.get_vision_model()
+        self.reasoning_model = model.get_reasoning_model()
+        self.formatting_model = model.get_formatting_model()
+        self.stream_handler = stream_handler
+        
+    @traceable
+    def run_full_chain(self, images: List[str]):
+        """Runs the full chain of image analysis and chat models."""
+        food_items = set()
 
-def stream_chain(chain: Callable, params_dict: dict) -> Generator:
-    """Streams a chain of operations and yields the result."""
-    for chunk in chain.stream(params_dict):
-        yield chunk
+        self.stream_handler.write("Looking for food ingredients...", True)
 
-def handle_json(output: List) -> str:
-    """Handles a JSON output and returns the result."""
-    return json.dumps(output[-1])
+        for img in images:
+            answer = ImageAnswer.parse_raw(self._analyzeImage(img))
+            for item in answer.answer : 
+                food_items.add(item)
+        
+        if len(food_items) == 0:
+            self.stream_handler.write("Sorry - couldn't find any food ingredients in the images :(", True)
+            return
+
+        self.stream_handler.write("Found the following food items:", True)
+        ingredients = ", ".join(food_items)
+        self._format_ingredients(ingredients)
+
+        self.stream_handler.write("RatatouAIng 3 recipes with these ingredients...", True) 
+
+        recipes = self._getRecipes(ingredients)
+
+        for recipe in recipes:
+            recipe = recipe.json()
+
+            missing_ingredients_prompt = self._get_prompt_template(prompts.system_ingredients_gaps_prompt_template, 
+                                                            prompts.human_ingredients_gaps_prompt_templatee)
+            reflection_prompt = self._get_prompt_template(prompts.system_recipe_reflection_prompt_templatee, 
+                                                    prompts.human_recipe_reflection_prompt_template)
+            formatted_recipe_prompt = self._get_prompt_template(prompts.system_content_formatting_prompt_template, 
+                                                        prompts.human_final_formatting_prompt_template)
+
+            missing_ingredients = ({"recipe": RunnablePassthrough(),"ingredients_list": RunnablePassthrough() } | 
+                                missing_ingredients_prompt | self.reasoning_model | StrOutputParser())
+            reflection = ({"recipe": RunnablePassthrough()} | reflection_prompt | self.reasoning_model | StrOutputParser())
+            chain = ({"missing_ingredients": missing_ingredients, "reflection" : reflection, "recipe": RunnablePassthrough()} | 
+                    formatted_recipe_prompt | self.formatting_model | StrOutputParser())
+
+            self.stream_handler.stream(self._stream_chain(chain, {"recipe": recipe, "ingredients_list": ingredients}), True) 
+        
+        self.stream_handler.close()
+
+
+    @traceable
+    def _analyzeImage(self, img: str) -> ImageAnswer:
+        """Analyzes an image and returns an ImageAnswer object."""
+        system_message = SystemMessagePromptTemplate.from_template(prompts.system_img2ingredients_prompt_template)
+
+        human_message = HumanMessage(
+            content=[
+                {"type": "text", "text": "Here is an image:\n"},
+                {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{img}", },},
+                {"type": "text", "text": "\n" + prompts.human_img2ingredients_prompt_template}, 
+                ])
+
+        chat_prompt = ChatPromptTemplate.from_messages([system_message, human_message])
+        output_parser = JsonOutputParser(pydantic_object=ImageAnswer, diff=True)
+        
+        chain = chat_prompt | self.vision_model | output_parser
+        return self._handle_json(self.stream_handler.stream(self._stream_chain(chain, {})))
+
+
+    @traceable
+    def _format_ingredients(self, ingredients: str):
+        """Formats the ingredients and returns the result."""
+        result = self._execute_chain(prompts.system_content_formatting_prompt_template, 
+                        prompts.human_ingredients_formatting_prompt_template, self.formatting_model, 
+                        StrOutputParser(), {"ingredients": ingredients}, True)
+        return result
+
+    @traceable
+    def _getRecipes(self, ingredients: str) -> RecipesLists:
+        """Gets recipes based on the ingredients and returns a RecipesLists object."""
+        recipes = self._execute_chain(prompts.system_ingredients2recipe_prompt_template, 
+                        prompts.human_ingredients2recipe_prompt_template, self.reasoning_model, 
+                        JsonOutputParser(pydantic_object=RecipesLists, diff=True), 
+                        {"ingredients_list": ingredients, "recipe_answer_outputformat" : prompts.recipeslist_answer_outputformat})
+
+        return RecipesLists.parse_raw(json.dumps(recipes)).recipes_lists
+
+    @traceable 
+    def _get_prompt_template(self, system_template, human_template):
+        """Gets a prompt template based on the system and human templates."""
+        system_message = SystemMessagePromptTemplate.from_template(system_template)
+        human_message = HumanMessagePromptTemplate.from_template(human_template)
+
+        return ChatPromptTemplate.from_messages([system_message, human_message])
+
+    @traceable 
+    def _execute_chain(self, system_template: str, human_template: str, chat:BaseLLM, output_parser: Callable, 
+                    params_dict: dict, persist: bool = False) -> str:
+        """Executes a chain of operations and returns the result."""
+        chat_prompt = self._get_prompt_template(system_template, human_template)
+        chain = chat_prompt | chat | output_parser
+
+        return self.stream_handler.stream(self._stream_chain(chain, params_dict), persist)
+
+    def reply(self, user_prompt: str) -> str:
+        """Replies to a user prompt and returns the result. Includes the last 10~ messages in the conversation."""
+        chat=self.reasoning_model
+        messages = [(m["role"], m["content"]) for m in self.stream_handler.messages if m["type"] == "text"]
+
+        promptTmplt = ChatPromptTemplate.from_messages(messages)  
+        chain = promptTmplt | chat | StrOutputParser()
+        return self.stream_handler.stream(self._stream_chain(chain, {}), True)
+
+    def _stream_chain(self, chain: Callable, params_dict: dict) -> Generator:
+        """Streams a chain of operations and yields the result."""
+        try:
+            for chunk in chain.stream(params_dict):
+                yield chunk
+        except APIStatusError as ase:
+            logging.error("An error occurred while calling the model:", exc_info=True)
+        except Exception as e:
+            logging.error("An error occurred while calling the model:", exc_info=True)
+            return
+        
+    def _stream_jsonoutput_chain(self, chain: Callable, params_dict: dict) -> Generator:
+        """Streams a chain of operations and yields the result."""
+        initial_json={}
+        try:
+            for diff in chain.stream(params_dict):
+
+                yield jsonpatch.apply_patch(initial_json, diff)
+
+        except APIStatusError as ase:
+            logging.error("An error occurred while calling the model:", exc_info=True)
+        except Exception as e:
+            logging.error("An error occurred while calling the model:", exc_info=True)
+            return
+
+    def _handle_json(self, output: List) -> str:
+        """Handles a JSON output and returns the result."""
+        return json.dumps(output)
